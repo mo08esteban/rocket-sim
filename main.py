9eb0a9593a0bc549daf8e0977f1c2df83f2d4652
Esteban Mo <mo08.esteban@proton.me>
2026-03-23 00:42:40 +0000

    V0.3: 加入燃料消耗（质量随时间减少）

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
 # 参数
+mdot = 0.8
 g = 9.8
 thrust = 2000
 mass = 100
@@ -16,16 +17,16 @@ height_list = []
 
 # 模拟循环
 while t <= t_max:
-    # 计算加速度 a
+    # 质量减少（燃料消耗）
+    mass = mass - mdot * dt
+    # 如果质量变成负数，就停止（燃料耗尽）
+    if mass <= 0:
+        break
     a = thrust / mass - g
-    # 更新速度 v
     v = v + a * dt
-    # 更新高度 h
     h = h + v * dt
-    # 记录时间和高度
     time_list.append(t)
     height_list.append(h)
-    # 更新时间
     t = t + dt
 
 plt.plot(time_list, height_list)
